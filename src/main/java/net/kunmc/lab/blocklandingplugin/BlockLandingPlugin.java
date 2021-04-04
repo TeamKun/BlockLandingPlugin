0c1de35402470a42ea43ddb6620f0f7d118f5341
sudorino <sudorino0909@gmail.com>
2021-04-04 12:39:25 +0900

    first commit

---
+++ src/main/java/net/kunmc/lab/blocklandingplugin/BlockLandingPlugin.java
@@ -0,0 +1,17 @@
+package net.kunmc.lab.blocklandingplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BlockLandingPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
